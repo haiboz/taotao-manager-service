765273b944f72b428a748994c32b88e7f17549b7
haiboz <798102408@qq.com>
2016-10-18 18:13:00 +0800

    内容分类新增

--- src/main/java/com/taotao/service/ContentCategoryService.java
+++ src/main/java/com/taotao/service/ContentCategoryService.java
@@ -2,6 +2,7 @@ package com.taotao.service;
 
 import java.util.List;
 import com.taotao.common.pojo.EUTreeNode;
+import com.taotao.common.pojo.TaotaoResult;
 
 /**
  * 内容分类
@@ -15,4 +16,12 @@ public interface ContentCategoryService {
 	 * @return
 	 */
 	public List<EUTreeNode> getCategoryList(long parentId);
+	
+	/**
+	 * 新增新的内容分类
+	 * @param parentId
+	 * @param name
+	 * @return
+	 */
+	public TaotaoResult insertContentCategory(long parentId,String name);
 }
--- src/main/java/com/taotao/service/impl/ContentCategoryServiceImpl.java
+++ src/main/java/com/taotao/service/impl/ContentCategoryServiceImpl.java
@@ -1,12 +1,14 @@
 package com.taotao.service.impl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.EUTreeNode;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.mapper.TbContentCategoryMapper;
 import com.taotao.pojo.TbContentCategory;
 import com.taotao.pojo.TbContentCategoryExample;
@@ -32,6 +34,29 @@ public class ContentCategoryServiceImpl implements ContentCategoryService {
 			listTree.add(node);
 		}
 		return listTree;
+	}
+
+	@Override
+	public TaotaoResult insertContentCategory(long parentId, String name) {
+		TbContentCategory contentCategory = new TbContentCategory();
+		contentCategory.setName(name);
+		contentCategory.setParentId(parentId);
+		contentCategory.setSortOrder(1);
+		contentCategory.setCreated(new Date());
+		contentCategory.setUpdated(new Date());
+		contentCategory.setIsParent(false);
+		contentCategory.setStatus(1);
+		mapper.insert(contentCategory);
+		//查看父节点的isparent是否为true  如果不是 则置为true
+		
+		TbContentCategory parentTb = mapper.selectByPrimaryKey(parentId);
+		if(!parentTb.getIsParent()){
+			parentTb.setIsParent(true);
+			mapper.updateByPrimaryKey(parentTb);
+		}
+		//返回结果
+		return TaotaoResult.ok(contentCategory);
+		
 	}
 
 }
