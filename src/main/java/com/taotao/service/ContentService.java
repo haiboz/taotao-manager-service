d58573c1f415c3c968db243603fdebe81aaed6a1
haiboz <798102408@qq.com>
2016-10-20 17:12:22 +0800

    首页大广告位展示 广告内容添加

--- src/main/java/com/taotao/service/ContentService.java
+++ src/main/java/com/taotao/service/ContentService.java
@@ -1,7 +1,23 @@
 package com.taotao.service;
 
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbContent;
 
 public interface ContentService {
+	/**
+	 * 分页查询内容列表
+	 * @param categoryId
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
 	public EUDataGridResult getContentList(Long categoryId,int page ,int rows);
+	
+	/**
+	 * 保存内容信息
+	 * @param tbContent
+	 * @return
+	 */
+	public TaotaoResult saveContent(TbContent tbContent);
 }
--- src/main/java/com/taotao/service/impl/ContentServiceImpl.java
+++ src/main/java/com/taotao/service/impl/ContentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.taotao.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +8,26 @@ import org.springframework.stereotype.Service;
 
 import com.taotao.bo.ContentBo;
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
+import com.taotao.pojo.TbContentExample;
 import com.taotao.service.ContentService;
 
 @Service
 public class ContentServiceImpl implements ContentService {
 	@Autowired
 	private ContentBo contentBoImpl;  
+	@Autowired
+	private TbContentMapper mapper;
 	
 	
 	@Override
 	public EUDataGridResult getContentList(Long categoryId,int page,int rows) {
-		int total = contentBoImpl.getContentCount(categoryId);
+//		int total = contentBoImpl.getContentCount(categoryId);
+		TbContentExample example = new TbContentExample();
+		example.createCriteria().andCategoryIdEqualTo(categoryId);
+		int total = mapper.countByExample(example);
 		int begin = (page-1)*rows;
 		List<TbContent> list = contentBoImpl.getContentPageList(categoryId, begin, rows);
 		EUDataGridResult result = new EUDataGridResult();
@@ -26,5 +35,15 @@ public class ContentServiceImpl implements ContentService {
 		result.setTotal(total);
 		return result;
 	}
+
+	@Override
+	public TaotaoResult saveContent(TbContent tbContent) {
+		tbContent.setCreated(new Date());
+		tbContent.setUpdated(new Date());
+		mapper.insert(tbContent);
+		return TaotaoResult.ok();
+	}
+	
+	
 
 }
